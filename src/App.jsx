a348f35a49081c7079b0797a519bee0f253fe05f
Benbow <benrocks2005@gmail.com>
2025-03-01 15:44:43 -0500

    Added content to most files

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import Dashboard from './Components/Dashboard';
 import MapView from './Components/MapView';
 import Navbar from './Components/Navbar';
 import { Route, Routes } from 'react-router-dom';
+import Context from './Components/Context';
 
 function App() {
+  const [reports, setReports] = useState([
+      {id: 0, severity: 5, crime: "I saw someone", location: "AMERICA", safety: "Close eyes"},
+    ]);
+
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Dashboard/>}/>
-      <Route path='/Report' element={<AnalyzeCrime/>}/>
-      <Route path="/map" element={<MapView/>}/>
-    </Routes>
+    <Context.Provider value={{ reports, setReports }}>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/Report' element={<AnalyzeCrime/>}/>
+        <Route path="/map" element={<MapView/>}/>
+      </Routes>
+    </Context.Provider>
     </>
   )
 }
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import Context from './Context';
 
 function Dashboard() {
 
+  const {reports, setReports} = useContext(Context);
   return (
     <>
         <div>
             <h1>Welcome to CrimeScope™</h1>
-            <p>Below you will see a list of crimes in your local region</p>
+            <p>Below you will see a list of local reports</p>
+        </div>
+        <div>
+            <ul>
+                {reports.map((report) => (
+                  <li>{report.crime}</li>
+                ))}
+            </ul>
         </div>
     </>
     
