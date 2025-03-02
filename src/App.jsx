b12eb95722b728bc78e13d18dbe445ff08cf2c2c
Benbow <benrocks2005@gmail.com>
2025-03-01 22:07:54 -0500

    Fixed some conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './styles/App.css';
-import './styles/Navbar.css'
-import './styles/index.css'
-import React, {useState} from 'react';
+import './App.css';
+import './styles/Navbar.css';
+import './index.css';
+import React, { useState } from 'react';
 import AnalyzeCrime from './Components/CrimeSeverityRating';
 import Dashboard from './Components/Dashboard';
 import Navbar from './Components/Navbar';
@@ -10,18 +10,20 @@ import Context from './Components/Context';
 
 function App() {
   const [reports, setReports] = useState([
-      {id: 0, severity: 5, crime: "I saw someone", location: "AMERICA", safety: "Close eyes"},
+    { id: 0, severity: 5, crime: "I saw someone", location: "AMERICA", safety: "Close eyes" },
   ]);
   const [showMap, setShowMap] = useState(false);
   return (
     <div>
       <Context.Provider value={{ reports, setReports, showMap, setShowMap }}>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Dashboard/>}/>
-          <Route path='/Report' element={<AnalyzeCrime/>}/>
-          {/* <Route path="/map" element={<MapView/>}/> */}
-        </Routes>
+        <Navbar />
+        <div className='main'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/Report' element={<AnalyzeCrime />} />
+          </Routes>
+        </div>
+
       </Context.Provider>
     </div>
   )
